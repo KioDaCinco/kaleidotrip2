e1baa6475827d90c32d1c227a0f4ccb29779858a
KioDaCinco <KioSurla18@gmail.com>
2025-10-22 21:31:38 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ window.onclick = function(event) {
   if (event.target == signUpModal) signUpModal.style.display = "none";
 };
 
-// Fake authentication
+// Fake authentication (front-end only)
 document.getElementById("submitSignIn").onclick = () => {
   const email = document.getElementById("signInEmail").value;
   const pass = document.getElementById("signInPassword").value;
